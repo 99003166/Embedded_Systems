d3050b21262fb7f8dc742e441a11e04982f78d05
99003166 <75420713+99003166@users.noreply.github.com>
2021-01-02 15:45:43 +0530

    Add files via upload

---
+++ Activity_1/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+int operand1 = 7, operand2 = 6,operand3 = 1,operand4 = 2,operand5 = 5;
+int output;
+output = (operand1 + operand2)*operand3-(operand4^operand5);
+printf("The sum of two numbers is", output);
+return 0;
+}
